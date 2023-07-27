a2c3c0372e54c8ddf40b3f0c421d5acb1fa500f3
FadeScripting <79726377+Sheepy9999@users.noreply.github.com>
2023-07-27 18:17:23 +0200

    Create FadePaid.lua

---
+++ script/paids/dahood/FadePaid.lua
@@ -0,0 +1,15 @@
+--[[
+,gggggggggggggg                                   ,ggggggggggg,                             
+dP""""""88""""""                  8I              dP"""88""""""Y8,                        8I 
+Yb,_    88                        8I              Yb,  88      `8b                        8I 
+ `""    88                        8I               `"  88      ,8P           gg           8I 
+     ggg88gggg                    8I                   88aaaad8P"            ""           8I 
+        88   8  ,gggg,gg    ,gggg,8I   ,ggg,           88"""""    ,gggg,gg   gg     ,gggg,8I 
+        88     dP"  "Y8I   dP"  "Y8I  i8" "8i          88        dP"  "Y8I   88    dP"  "Y8I 
+  gg,   88    i8'    ,8I  i8'    ,8I  I8, ,8I          88       i8'    ,8I   88   i8'    ,8I 
+   "Yb,,8P   ,d8,   ,d8b,,d8,   ,d8b, `YbadP'          88      ,d8,   ,d8b,_,88,_,d8,   ,d8b,
+     "Y8P'   P"Y8888P"`Y8P"Y8888P"`Y8888P"Y888         88      P"Y8888P"`Y88P""Y8P"Y8888P"`Y8
+
+--]]
+_Z82nZ8218Zj, Protected_by_Fadesucator, Discord = 'https://discord.gg/NCsRnp6ZdP'
+                                                                                             
